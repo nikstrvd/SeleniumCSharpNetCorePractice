1b792a7e6ec276ee71b4a5a2b3567d6254d44d33
nikhil <nikstrvd123@gmail.com>
2021-07-21 13:05:26 +0530

    Initial Commit
    Initial Commit

---
+++ SeleniumCsharpNetCore/UnitTest1.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace SeleniumCsharpNetCore
+{
+    public class Tests : DriverHelper
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+            Console.WriteLine("Setup");
+            Driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Driver.Navigate().GoToUrl("http://backend.tmsapp.2stallions.site/login");
+            Driver.Manage().Window.Maximize();
+            Driver.FindElement(By.Id("email")).SendKeys("superadmin@mail.com");
+            Driver.FindElement(By.Id("password")).SendKeys("123456");
+            Driver.FindElement(By.XPath("//button")).Click();
+            Console.WriteLine("Test1");
+            Assert.Pass();
+            Driver.Quit();
+        }
+    }
+}
